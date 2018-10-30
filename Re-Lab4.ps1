0d475e552e0788b589794ad37acf333aea831732
thegib00 <44264324+thegib00@users.noreply.github.com>
2018-10-30 15:09:00 -0500

    Fixed Everything
    This fixes the loop.

--- Re-Lab4.ps1
+++ Re-Lab4.ps1
@@ -3,8 +3,11 @@
 #Create a folder for each of the users in the csv, the folder name should be their name with no spaces or capitalization
 #Inside each of their folders you should create a hello.txt file that gives them a welcome message
 
-new-item "c:\fileserver" -type "directory"
-$csv = import-csv "c:\employeelist.csv"
-foreach ($_ in $csv) {
-    new-item  -type "directory"
+new-item "e:\fileserver" -type "directory"
+$csv = import-csv "e:\employeelist.csv"
+
+foreach ($line in $csv.name.tolower().replace(' ','')) {
+    new-item -type "directory" -name "$line" -path "e:\fileserver\"
+    new-item -name "hello.txt" -path "e:\fileserver\$line" 
+    add-content -value "Welcome to the File Server!" -path "e:\fileserver\$line\hello.txt"
 }
